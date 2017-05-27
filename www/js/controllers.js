2edbf7563030a8a48eaa566bd72cf96268ea0514
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2017-05-27 21:52:12 +0200

    [FEAT]: added firebase && angularfire and connected from home

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,13 +6,27 @@ angular.module('starter.controllers', [])
   console.log('app controller');
 })
 
-.controller('HomeController', function($interval, AppService, $cordovaGeolocation, $ionicPlatform) {
+.controller('HomeController', function($scope, $interval, AppService, $cordovaGeolocation, $ionicPlatform, $firebaseArray) {
 
   var vm = this;
   vm.history = [{
     lat: 1234,
     long: 1234
   }];
+
+  var config = {
+    apiKey: "firebase config here",
+    authDomain: "firebase config here",
+    databaseURL: "firebase config here",
+    projectId: "firebase config here",
+    storageBucket: "firebase config here",
+    messagingSenderId: "firebase config here"
+  };
+  var fireRef = firebase.initializeApp(config);
+
+  var ref = firebase.database().ref('channels');
+  vm.newslist = $firebaseArray(ref);
+
 
   $ionicPlatform.ready(function() {
 
